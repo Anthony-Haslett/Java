8e891289270c1ccc557bd9df19618ed8f7129902
Anthony Haslett <anthony-haslett@outlook.com>
2017-05-18 13:11:24 +0100

    new data

---
+++ ShopItemReservation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+ * Write a description of class ShopItemReservation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShopItemReservation
+{
+    private String reservationNo, itemID, CustomerID;
+    private Date startDate;
+    private int noOfDays;
+}

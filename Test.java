399345638eb89cbb985a4e77874e19fca269b933
Anthony Haslett <anthony-haslett@outlook.com>
2017-05-18 12:50:29 +0100

    x
    x

--- Test.java
+++ Test.java
@@ -5,22 +5,38 @@
  * @author (Anthony Haslett) 
  * @version (Part 3)
  */
+import javax.swing.JOptionPane;
 public class Test
 {
     public Test()
     {
-        /** Shop test*/
-        Shop shop = new Shop();
+        /** 1)Shop test*/
+       
+        //Shop shop = new Shop();
+        
         //         shop.readData();
+        
         //         shop.printAllDetailsMap();
-        /** Customer test*/   
-        Customer customer = new Customer("","Roberts","John","T","Mr");
-        shop.addCustomerMap(customer);
-
-        shop.printAllCustomers();
-        System.out.println("--- Customer Added to Array \n");
+        /** 2) Customer test (single customer)*/ 
+     
+//         Shop shop = new Shop();
+        
+//         Customer customer = new Customer("","Roberts","John","T","Mr");
+        
+//         shop.addCustomerMap(customer);
+//         shop.printAllCustomers();
+       
+//         JOptionPane.showMessageDialog(null, "Customer Added to Array");
+        /** 3) Customer test (All customers)*/ 
+     
+        Shop shop = new Shop();
+     
+        Customer customer = new Customer();
+     
         shop.readCustomerData();
+        
         shop.printAllCustomers();
+       
         shop.writeCustomerData();
     }  
 }
